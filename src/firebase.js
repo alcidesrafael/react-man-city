0d4cadbf40ff757f8fd2cf0a0efbd9d7088918a1
alcidesrafael <rafael4left@gmail.com>
2018-09-17 17:49:55 -0300

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/app'
+import 'firebase/database'
+
+const config = {
+    apiKey: "AIzaSyBv09E-5cODqRVobo_BIG0tXjxaM4CBfCo",
+    authDomain: "m-city-34563.firebaseapp.com",
+    databaseURL: "https://m-city-34563.firebaseio.com",
+    projectId: "m-city-34563",
+    storageBucket: "m-city-34563.appspot.com",
+    messagingSenderId: "532549266612"
+}
+
+firebase.initializeApp(config);
